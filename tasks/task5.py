d4f286ebf6b4b4365d3a1bfa5a6863eec47a7c65
student <student@corp.kpiot.ru>
2025-10-07 11:04:40 +0500

    hh

--- tasks/task5.py
+++ tasks/task5.py
@@ -2,7 +2,13 @@
 
 def solve():
 # Ниже пишите решение задачи
-
+    n=int(input())
+    n=n%86400
+    hours = n//3600
+    minutes=n//60
+    seconds=n%60
+    print(hours,':' ,minutes//10,minutes%10,':' ,seconds//10, seconds%10,sep="")
+    
 
    
 # Код ниже не трогать! он нужен для тестов
